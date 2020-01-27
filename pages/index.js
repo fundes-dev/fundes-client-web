58b41d004c84d58b2eb18270d936677aab8c0e4a
Can Rozanes <crozanes@omers.com>
2020-01-27 18:47:13 -0500

    changed text

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,9 @@ const Home = () => (
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
+      <h1 className="title">Welcome to Fundes!</h1>
       <p className="description">
-        To get started, edit
-        {' '}
-        <code>pages/index.js</code>
-        {' '}
-and save to reload.
-        {' '}
-
+        Patreon for NPM Packages!
       </p>
 
       <div className="row">
