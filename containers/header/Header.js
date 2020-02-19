13e3025db47dd7da893f20daa2455831dc21ab3e
Eugene Michasiw <eugene@michasiw.com>
2020-02-19 10:21:37 -0500

    fix header styling

--- containers/header/Header.js
+++ containers/header/Header.js
@@ -1,59 +1,26 @@
-import { Toolbar, AppBar, Typography } from '@material-ui/core';
-import { makeStyles, styled } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
+import AppBar from '../../components/AppBar';
+import Toolbar from '../../components/Toolbar';
 import Link from '../../components/Link';
+import TopNav from '../../components/TopNav';
 
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    justifyContent: 'space-between',
-  },
-  ul: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginLeft: 'auto',
-    listStyle: 'none',
-  },
-  li: {
-    marginRight: theme.spacing(3),
-  },
+const HeaderLogo = styled(Link)(({ theme }) => ({
+  color: theme.palette.primary.main,
+  textDecoration: 'none',
 }));
 
-const HeaderLogo = styled(Link)({
-  color: 'white',
-  textDecoration: 'none',
-});
-const HeaderLink = styled(Link)({
-  color: 'white',
-  textDecoration: 'none',
-  fontSize: '16px',
-});
-
-const Header = () => {
-  const classes = useStyles();
-
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h4">
-          <HeaderLogo href="/" color="primary">
+const Header = () => (
+  <AppBar>
+    <Toolbar>
+      <Typography variant="h4">
+        <HeaderLogo href="/" color="primary">
             Fundes
-          </HeaderLogo>
-        </Typography>
-        <ul className={classes.ul}>
-          <li className={classes.li}>
-            <HeaderLink href="/about">
-              About
-            </HeaderLink>
-          </li>
-          <li className={classes.li}>
-            <HeaderLink href="/my-account">
-              My Account
-            </HeaderLink>
-          </li>
-        </ul>
-      </Toolbar>
-    </AppBar>
-  );
-};
+        </HeaderLogo>
+      </Typography>
+      <TopNav />
+    </Toolbar>
+  </AppBar>
+);
 
 export default Header;
